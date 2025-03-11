4056967b84cfc9736e6d91c792809e34eebb7d67
Aziz Kanchwala <azizkanch435@gmail.com>
2025-03-11 10:26:55 +0530

    Controllable search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Navbar,Container } from "react-bootstrap";
 
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+};
+
 const Header = (props) => {
   return (
-    <Navbar bg="secondary" variant="light" expand="lg" className="p-2">
+    <Navbar style={navbarStyle} variant="light" expand="lg" className="p-2">
       <Container className="justify-content-center">
         <Navbar.Brand className="mx-auto">{props.title}</Navbar.Brand>
       </Container>
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({word, setWord, handleSubmit}) => {
   return (
     <Container className="mt-5">
-      <Row>
-        <Col>
-          <Form>
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Form onSubmit={handleSubmit}>
             <Row>
-              <Col>
-                <Form.Control placeholder="Search for new image" />
+              <Col xs={9}>
+                <Form.Control placeholder="Search for new image" 
+                type="text" value={word} onChange={(e) => setWord(e.target.value)}/>
               </Col>
               <Col>
                 <Button variant="primary" type="submit">Search</Button>
